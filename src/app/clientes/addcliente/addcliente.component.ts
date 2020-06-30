fd102908009ae30f5c3663808a2b40c8755097f5
slayne54 <slayne54@hotmail.com>
2020-06-30 13:22:54 +0200

    foirmulario addcliente terminado

--- src/app/clientes/addcliente/addcliente.component.ts
+++ src/app/clientes/addcliente/addcliente.component.ts
@@ -21,9 +21,20 @@ export class AddclienteComponent implements OnInit {
       razon: new FormControl('', [Validators.required, Validators.maxLength(50)]),
       direccion: new FormControl('', [Validators.required, Validators.maxLength(100)]),
       cp: new FormControl('', [Validators.required, Validators.maxLength(5)]),
-
+      poblacion: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+      provincia: new FormControl('valladolid', [Validators.required]),
+      pais: new FormControl('España', [Validators.required, Validators.maxLength(50)]),
+      telefono1: new FormControl('', [Validators.required, Validators.maxLength(9)]),
+      telefono2: new FormControl('', [Validators.required, Validators.maxLength(9)]),
+      fax: new FormControl('', [Validators.required, Validators.maxLength(9)]),
+      actividad: new FormControl('', [Validators.required, Validators.maxLength(150)]),
       email: new FormControl('', [Validators.required, Validators.maxLength(50), Validators.email]),
-      telefono: new FormControl('', [Validators.required, Validators.maxLength(50)])
+      fecha_alta: new FormControl('', [Validators.required, Validators.maxLength(15)]),
+      fecha_modif: new FormControl('', [Validators.required, Validators.maxLength(15)]),
+      observaciones: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+
+
+
 
     });
     this.addClienteForm.valueChanges.pipe(debounceTime(1000)).subscribe(valor => { console.log(valor) });
@@ -133,5 +144,115 @@ export class AddclienteComponent implements OnInit {
   get cpInValid() {
     //devuelve true si el campo name esta tocado y es invalido
     return this.cpField.touched && this.cpField.invalid;
+  }
+  get poblacionField() {
+    return this.addClienteForm.get('poblacion');
+  }
+  get poblacionValid() {
+    //devuelve true si el campo name esta tocado y es valido
+    return this.poblacionField.touched && this.poblacionField.valid;
+  }
+  get poblacionInValid() {
+    //devuelve true si el campo name esta tocado y es invalido
+    return this.poblacionField.touched && this.poblacionField.invalid;
+  }
+  get provinciaField() {
+    return this.addClienteForm.get('provincia');
+  }
+  get provinciaValid() {
+    //devuelve true si el campo name esta tocado y es valido
+    return this.provinciaField.touched && this.provinciaField.valid;
+  }
+  get provinciaInValid() {
+    //devuelve true si el campo name esta tocado y es invalido
+    return this.provinciaField.touched && this.provinciaField.invalid;
+  }
+  get paisField() {
+    return this.addClienteForm.get('pais');
+  }
+  get paisValid() {
+    //devuelve true si el campo name esta tocado y es valido
+    return this.paisField.touched && this.paisField.valid;
+  }
+  get paisInValid() {
+    //devuelve true si el campo name esta tocado y es invalido
+    return this.paisField.touched && this.paisField.invalid;
+  }
+  get telefono1Field() {
+    return this.addClienteForm.get('telefono1');
+  }
+  get telefono1Valid() {
+    //devuelve true si el campo name esta tocado y es valido
+    return this.telefono1Field.touched && this.telefono1Field.valid;
+  }
+  get telefono1InValid() {
+    //devuelve true si el campo name esta tocado y es invalido
+    return this.telefono1Field.touched && this.telefono1Field.invalid;
+  }
+  get telefono2Field() {
+    return this.addClienteForm.get('telefono2');
+  }
+  get telefono2Valid() {
+    //devuelve true si el campo name esta tocado y es valido
+    return this.telefono2Field.touched && this.telefono2Field.valid;
+  }
+  get telefono2InValid() {
+    //devuelve true si el campo name esta tocado y es invalido
+    return this.telefono2Field.touched && this.telefono2Field.invalid;
+  }
+  get faxField() {
+    return this.addClienteForm.get('fax');
+  }
+  get faxValid() {
+    //devuelve true si el campo name esta tocado y es valido
+    return this.faxField.touched && this.faxField.valid;
+  }
+  get faxInValid() {
+    //devuelve true si el campo name esta tocado y es invalido
+    return this.faxField.touched && this.faxField.invalid;
+  }
+  get actividadField() {
+    return this.addClienteForm.get('actividad');
+  }
+  get actividadValid() {
+    //devuelve true si el campo name esta tocado y es valido
+    return this.actividadField.touched && this.actividadField.valid;
+  }
+  get actividadInValid() {
+    //devuelve true si el campo name esta tocado y es invalido
+    return this.actividadField.touched && this.actividadField.invalid;
+  }
+  get fecha_altaField() {
+    return this.addClienteForm.get('fecha_alta');
+  }
+  get fecha_altaValid() {
+    //devuelve true si el campo name esta tocado y es valido
+    return this.fecha_altaField.touched && this.fecha_altaField.valid;
+  }
+  get fecha_altaInValid() {
+    //devuelve true si el campo name esta tocado y es invalido
+    return this.fecha_altaField.touched && this.fecha_altaField.invalid;
+  }
+  get fecha_modifField() {
+    return this.addClienteForm.get('fecha_modif');
+  }
+  get fecha_modifValid() {
+    //devuelve true si el campo name esta tocado y es valido
+    return this.fecha_modifField.touched && this.fecha_modifField.valid;
+  }
+  get fecha_modifInValid() {
+    //devuelve true si el campo name esta tocado y es invalido
+    return this.fecha_modifField.touched && this.fecha_modifField.invalid;
+  }
+   get observacionesField() {
+    return this.addClienteForm.get('observaciones');
+  }
+  get observacionesValid() {
+    //devuelve true si el campo name esta tocado y es valido
+    return this.observacionesField.touched && this.observacionesField.valid;
+  }
+  get observacionesInValid() {
+    //devuelve true si el campo name esta tocado y es invalido
+    return this.observacionesField.touched && this.observacionesField.invalid;
   }
 }

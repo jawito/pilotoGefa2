bcb343f935fd62095103c8f43b0f488303030754
Ivan Cubero <jawito@gmail.com>
2020-07-30 18:02:34 +0200

    ordena y busca

--- src/app/clientes/cliente-tabla/cliente-tabla.component.ts
+++ src/app/clientes/cliente-tabla/cliente-tabla.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
-import { ClientesService } from 'src/app/services/clientes.service';
+import { ClientesService, ClientePr } from 'src/app/services/clientes.service';
 
 @Component({
   selector: 'app-cliente-tabla',
@@ -17,6 +17,7 @@ export class ClienteTablaComponent implements OnInit {
   displayedColumns: string[] = ['nombre', 'dni', 'razon', 'direccion', 
         'poblacion', 'provincia','telefono1', 'email','actividad'];
   dataSource = new MatTableDataSource();
+  clientes = new Array<ClientePr>();
   busqueda:string;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
@@ -30,14 +31,20 @@ export class ClienteTablaComponent implements OnInit {
   }
 
 getData(){
-  this.clienteService.getClientes().subscribe(res=>{
+  this.clienteService.getMapClientes().subscribe(res=>{
+    console.log("res",res);
      this.dataSource.data = res;
   })
-  console.log("datasource",this.dataSource);
+//   this.clienteService.getMapClientes().subscribe(res=>{
+//     console.log("res",res);
+//     this.clientes = res;
+//  })
+//   console.log("clientes",this.clientes);
 }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
+    console.log(filterValue);
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
     if (this.dataSource.paginator) {

bfb3d92e53eab24d90be71090031e0dc9fd8a012
slayne54 <slayne54@hotmail.com>
2020-07-30 20:43:27 +0200

    tabla desplegable

--- src/app/clientes/cliente-tabla/cliente-tabla.component.ts
+++ src/app/clientes/cliente-tabla/cliente-tabla.component.ts
@@ -3,22 +3,33 @@ import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
 import { ClientesService, ClientePr } from 'src/app/services/clientes.service';
+import {SelectionModel} from '@angular/cdk/collections';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
 
 @Component({
   selector: 'app-cliente-tabla',
   templateUrl: './cliente-tabla.component.html',
-  styleUrls: ['./cliente-tabla.component.scss']
+  styleUrls: ['./cliente-tabla.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 
 
 export class ClienteTablaComponent implements OnInit {
 
   constructor (private clienteService: ClientesService){}
-  displayedColumns: string[] = ['nombre', 'dni', 'razon', 'direccion', 
-        'poblacion', 'provincia','telefono1', 'email','actividad'];
+  displayedColumns: string[] = ['nombre', 'razon', 'direccion',
+        'poblacion', 'provincia'];
   dataSource = new MatTableDataSource();
   clientes = new Array<ClientePr>();
   busqueda:string;
+  expandedElement: ClienteTablaComponent | null;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
@@ -51,6 +62,7 @@ getData(){
       this.dataSource.paginator.firstPage();
     }
   }
+
 }
 
 
